70285672c500057a2596edecb90a4d22908f166a
Hariharan <hariharan08112001@gmail.com>
2025-04-08 11:29:32 +0530

    new func introduced

--- AlamofireWrapper/AFWrapper.swift
+++ AlamofireWrapper/AFWrapper.swift
@@ -11,21 +11,37 @@ import Alamofire
 @objc public class AFWrapper: NSObject {
     
     @objc public override init() {
-        
+        super.init()
     }
     
-    @objc public func getAllUsers() async throws -> [UserModel] {
+    @objc public func getAllUsers() async throws -> NSArray {
         try await withCheckedThrowingContinuation { continuation in
             AF.request("https://dummyjson.com/users", method: .get, parameters: nil, encoding: URLEncoding.default)
                 .validate()
                 .responseDecodable(of: UserResponse.self) { response in
                     switch response.result {
                     case .success(let userResponse):
-                        continuation.resume(returning: userResponse.users)
+                        continuation.resume(returning: NSArray(array: userResponse.users))
                     case .failure(let error):
                         continuation.resume(throwing: error)
                     }
                 }
         }
+    }
+    
+    @objc public func getAllUsersWithCompletion(
+       completion: @escaping ([UserModel]) -> Void,
+       failure: @escaping (Error) -> Void
+    ) {
+        AF.request("https://dummyjson.com/users", method: .get, parameters: nil, encoding: URLEncoding.default)
+           .validate()
+           .responseDecodable(of: UserResponse.self) { response in
+               switch response.result {
+               case .success(let userResponse):
+                   completion(userResponse.users)
+               case .failure(let error):
+                   failure(error)
+               }
+           }
     }
 }
